3d6feb6e197db5bd7cd73319bb1da78095d7839e
Sergei Pankov <sergeipankov98@mail.ru>
2023-07-21 21:05:13 +0400

    Update

---
+++ scripts/data.js
@@ -0,0 +1,90 @@
+export const data = [
+    {
+        src: './images/firstPhoto.jpg',
+        name: 'Первое совместное фото'
+    },
+    {
+        src: './images/goFromWork.jpg',
+        name: 'Едем домой с работы'
+    },
+    {
+        src: './images/date.jpg',
+        name: 'Чудесное свидание'
+    },
+    {
+        src: './images/lookToThefuture.jpg',
+        name: 'Смотрим в светлое будущее на плакате cccр'
+    },
+    {
+        src: './images/onStation.jpg',
+        name: 'На остановке'
+    },
+    {
+        src: './images/beach.jpg',
+        name: 'Сходили и на пляж'
+    },
+    {
+        src: './images/byAphone.jpg',
+        name: 'Купили новый телефон, съели шримп роллы'
+    },
+    {
+        src: './images/sport.jpg',
+        name: 'Готовимся к спорту!'
+    },
+    {
+        src: './images/whatchmeplaying.jpg',
+        name: 'Смотрит как я играю'
+    },
+    {
+        src: './images/inMoscow.jpg',
+        name: 'В Москву!'
+    },
+    {
+        src: './images/Moscow.jpg',
+        name: 'В Москве!'
+    },
+    {
+        src: './images/from Moscow.jpg',
+        name: 'Из Москвы!'
+    },
+    {
+        src: './images/drink.jpg',
+        name: 'Выпиваем? Согреваемся!'
+    },
+    {
+        src: './images/inHome.jpg',
+        name: 'У меня'
+    },
+    {
+        src: './images/pumpkinPie.jpg',
+        name: 'Готовим пирог!'
+    },
+    {
+        src: './images/photo_2023-07-21_20-33-07.jpg',
+        name: 'Едет в Лагерь'
+    },
+    {
+        src: './images/photo_2023-07-21_20-41-02.jpg',
+        name: 'Остановились '
+    },
+    {
+        src: './images/photo_2023-07-21_20-33-03.jpg',
+        name: 'Красотка на фоне... мусора :/'
+    },
+    {
+        src: './images/wait.jpg',
+        name: 'Жду её из лагеря'
+    },
+    {
+        src: './images/photo_2023-07-21_20-32-58.jpg',
+        name: 'Лето с тобой '
+    },
+    {
+        src: './images/beach2.jpg',
+        name: 'Снова на пляже'
+    },
+    {
+        src: './images/next.jpg',
+        name: 'Что будет дальше?'
+    },
+]
